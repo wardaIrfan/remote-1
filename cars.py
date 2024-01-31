740450aac6c16b19092befe1e09c8d5cddc9cfa2
Warda Irfan <60103186@udst.edu.qa>
2024-01-31 11:09:37 +0300

    local-2 made changes

--- cars.py
+++ cars.py
@@ -3,5 +3,11 @@ def carSelection():
     cars=["Nissan","Hyundai","Honda","Suzuki","Kia"]
     selectedCar=random.choice(cars)
     print(selectedCar)
+
+def modelYear():
+    model=["1998","2001","2005","2010","2012","2022","2024"]
+    randomModel=random.choice(model)
+    print(randomModel)
     
 carSelection()
+modelYear()
